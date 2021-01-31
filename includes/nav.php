dc5fa46583e0c638524f05039c667d051f7ac0f7
iamstr <asatar1994@gmail.com>
2021-02-01 02:11:37 +0300

    working fine so far

---
+++ includes/nav.php
@@ -0,0 +1,26 @@
+ <!-- Navigation -->
+          <ul class="navbar-nav mb-md-3"> 
+          
+             <li class="nav-item">
+              <a class="nav-link" href="profile">
+                <i class="ni ni-single-02 text-dark"></i>
+                <span class="nav-link-text">Profile</span>
+              </a>
+            </li>
+            
+           
+            <li class="nav-item">
+              <a class="nav-link" href="register">
+                <i class="ni ni-circle-08 text-dark"></i>
+                <span class="nav-link-text">Register</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="upgrade">
+                <i class="ni ni-single-copy-04 text-dark"></i>
+                <span class="nav-link-text">Tasks</span>
+              </a>
+            </li>
+            
+          </ul>
+          
---
+++ includes/sidebar.php
@@ -0,0 +1,105 @@
+ <!-- Sidenav -->
+  <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+      <!-- Brand -->
+      <div class="sidenav-header  align-items-center">
+        <a class="navbar-brand" href="javascript:void(0)">
+          <img src="assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+        </a>
+      </div>
+      <div class="navbar-inner">
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="dashboard">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="icons">
+                <i class="ni ni-fat-add text-orange"></i>
+                <span class="nav-link-text">New Girl</span>
+              </a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="tables">
+                <i class="ni ni-bullet-list-67 text-success"></i>
+                <span class="nav-link-text">View Girls</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="map">
+                <i class="ni ni-books text-primary"></i>
+                <span class="nav-link-text">School</span>
+              </a>
+            </li>
+
+
+
+
+
+
+
+            <li class="nav-item">
+              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
+                <i class="ni ni-laptop text-orange"></i>
+                <span class="nav-link-text">Musaned</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
+                <i class="ni ni-archive-2 text-primary"></i>
+                <span class="nav-link-text"> NEA</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
+                <i class="ni ni-briefcase-24 text-info"></i>
+                <span class="nav-link-text">Enjaz</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
+                <i class="ni ni-check-bold text-primary"></i>
+                <span class="nav-link-text">Clearance</span>
+              </a>
+            </li>
+
+             <li class="nav-item">
+              <a class="nav-link" href="login">
+                <i class="ni ni-folder-17 text-info"></i>
+                <span class="nav-link-text">Travel</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link " href="upgrade.html">
+                <i class="ni ni-image text-danger"></i>
+                <span class="nav-link-text">Upload Doc </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="upgrade.html">
+                <i class="ni ni-send text-warning"></i>
+                <span class="nav-link-text">Send CV </span>
+              </a>
+            </li>
+
+
+
+           
+          </ul>
+          <!-- Divider -->
+          <hr class="my-3">
+          <!-- Heading -->
+          <h6 class="navbar-heading p-0 text-muted">
+            <span class="docs-normal">Processes</span>
+          </h6>
+         <?php include("nav.php");?>
+        </div>
+      </div>
+    </div>
+  </nav>
